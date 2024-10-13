b48cc84f13e3396a7e84040c65c1a542ef0415a4
EPats <elliottapaterson@gmail.com>
2024-10-13 20:47:06 +0100

    FirstCommit

---
+++ script.js
@@ -0,0 +1,21 @@
+const blob = document.getElementById("blob");
+
+window.onmousemove = (e) => {
+  
+  const { clientX, clientY } = event
+  blob.animate({
+    left: `${clientX}px`,
+    top: `${clientY}px`
+    }, { duration: 2100, fill: "forwards" });
+  
+  for (const card of document.getElementsByTagName("figure")) {
+    
+    const rect = card.getBoundingClientRect(),
+      x = e.clientX - rect.left,
+      y = e.clientY - rect.top;
+
+    card.style.setProperty("--mouse-x", `${x}px`);
+    card.style.setProperty("--mouse-y", `${y}px`);
+  }
+  
+};
